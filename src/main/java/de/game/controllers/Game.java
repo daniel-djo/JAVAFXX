104f511a7d6d6f9c1ed07143e76d1a733567cdbe
Daniel <s.daniel@djokic.eu>
2024-07-10 17:29:50 +0200

    Added fields

---
+++ src/main/java/de/game/controllers/Game.java
@@ -0,0 +1,13 @@
+package de.game.controllers;
+
+import de.game.model.Player;
+
+public class Game {
+
+    public Game() {
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+    }
+
+    // Weitere Spiel-Logik
+}
