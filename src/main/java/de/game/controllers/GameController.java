df62701062da04d6def1a1e3cfac66e7f670ab89
Daniel <s.daniel@djokic.eu>
2024-07-10 18:33:23 +0200

    Created Startmenu

---
+++ src/main/java/de/game/controllers/GameController.java
@@ -0,0 +1,30 @@
+package de.game.controllers;
+
+import de.game.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import java.io.IOException;
+
+public class GameController {
+
+    @FXML
+    private GridPane gameGrid;
+
+    @FXML
+    public void initialize() {
+        // Initialisierungslogik
+        for (int row = 0; row < 10; row++) {
+            for (int col = 0; col < 10; col++) {
+                Button button = new Button();
+                button.setPrefSize(50, 50);
+                gameGrid.add(button, col, row);
+            }
+        }
+    }
+
+    @FXML
+    private void switchToMainMenu() throws IOException {
+        Main.setRoot("mainMenu");
+    }
+}
