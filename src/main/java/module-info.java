52d54060dfc000d1ab3b97678cf257cf382007a7
Daniel <s.daniel@djokic.eu>
2024-07-10 17:08:34 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module de.game.javafxx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens de.game.javafxx to javafx.fxml;
+    exports de.game.javafxx;
+}
