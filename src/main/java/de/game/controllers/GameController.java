94a36bf81d11f4361228c35619512ee2366b060b
Chris-is-Github <chdkraus@gmail.com>
2024-07-11 13:58:38 +0200

    maps

--- src/main/java/de/game/controllers/GameController.java
+++ src/main/java/de/game/controllers/GameController.java
@@ -1,6 +1,8 @@
 package de.game.controllers;
 
 import de.game.Main;
+import de.game.model.Tile;
+import de.game.model.TileType;
 import de.game.model.Player;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -26,13 +28,16 @@ public class GameController {
         Player player2 = new Player("Player 2");
     }
 
-    public void createGrid(int rows, int cols) {
+    public void createGrid(char[][] map) {
+        int rows = map.length;
+        int cols = map[0].length;
         gameGrid.getChildren().clear();
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
-                Button button = new Button();
-                button.setPrefSize(50, 50);
-                gameGrid.add(button, col, row);
+                TileType type = TileType.fromSymbol(map[row][col]);
+                Tile tile = new Tile(type);
+                tile.setPrefSize(50, 50);
+                gameGrid.add(tile, col, row);
             }
         }
     }
--- src/main/java/de/game/controllers/GameMapSelectionController.java
+++ src/main/java/de/game/controllers/GameMapSelectionController.java
@@ -3,6 +3,7 @@ package de.game.controllers;
 import java.io.IOException;
 
 import de.game.Main;
+import de.game.model.TileType;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
@@ -15,23 +16,70 @@ public class GameMapSelectionController {
 
     @FXML
     private void createMapLI() throws IOException {
-        switchToGame(19, 10);
+        char[][] map = {
+            {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
+            {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'P', 'W', 'P', 'P', 'P', 'W', 'S'},
+            {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'S'},
+            {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'M', 'P', 'P', 'P', 'W', 'P', 'P', 'P', 'P', 'P', 'P', 'S'},
+            {'S', 'S', 'S', 'S', 'S', 'W', 'P', 'W', 'W', 'P', 'P', 'P', 'P', 'W', 'P', 'W', 'P', 'S', 'S'},
+            {'S', 'S', 'W', 'P', 'P', 'P', 'W', 'P', 'P', 'P', 'W', 'W', 'W', 'P', 'S', 'S', 'S', 'S', 'S'},
+            {'S', 'P', 'P', 'P', 'P', 'P', 'W', 'W', 'P', 'P', 'P', 'M', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
+            {'S', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
+            {'S', 'W', 'P', 'P', 'P', 'W', 'P', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
+            {'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'}
+        };
+        switchToGame(map);
     }
 
     @FXML
     private void createMapES() throws IOException {
-        switchToGame(19, 17); 
+        char[][] map = {
+            {'W', 'P', 'P', 'P', 'W', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'W'},
+            {'P', 'P', 'P', 'P', 'S', 'S', 'M', 'W', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+            {'P', 'P', 'P', 'P', 'S', 'S', 'S', 'W', 'P', 'P', 'P', 'P', 'W', 'P', 'P', 'P', 'P', 'P', 'W'},
+            {'P', 'P', 'W', 'S', 'S', 'S', 'S', 'S', 'W', 'W', 'S', 'S', 'P', 'P', 'P', 'W', 'P', 'P', 'P'},
+            {'S', 'P', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'M', 'W', 'P', 'P', 'P', 'M', 'P'},
+            {'P', 'P', 'P', 'P', 'W', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'W', 'P', 'W', 'P', 'W', 'P', 'P'},
+            {'P', 'P', 'P', 'P', 'W', 'S', 'S', 'P', 'P', 'S', 'S', 'S', 'W', 'P', 'P', 'P', 'P', 'P', 'P'},
+            {'P', 'P', 'P', 'P', 'P', 'S', 'P', 'P', 'S', 'S', 'S', 'S', 'W', 'P', 'W', 'P', 'P', 'W', 'P'},
+            {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'S', 'S', 'S', 'S', 'S', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+            {'P', 'M', 'P', 'P', 'W', 'W', 'S', 'S', 'S', 'S', 'S', 'P', 'P', 'S', 'W', 'P', 'P', 'P', 'P'},
+            {'P', 'P', 'P', 'P', 'P', 'W', 'S', 'S', 'S', 'S', 'P', 'P', 'S', 'S', 'W', 'P', 'P', 'P', 'P'},
+            {'P', 'P', 'P', 'W', 'P', 'W', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'P', 'P', 'M'},
+            {'M', 'P', 'P', 'P', 'P', 'P', 'M', 'W', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'P', 'S'},
+            {'P', 'W', 'P', 'P', 'W', 'P', 'P', 'P', 'P', 'W', 'P', 'S', 'S', 'S', 'S', 'S', 'P', 'P', 'P'},
+            {'P', 'P', 'P', 'P', 'P', 'P', 'W', 'P', 'P', 'P', 'W', 'P', 'S', 'S', 'S', 'S', 'W', 'P', 'P'},
+            {'W', 'P', 'P', 'P', 'P', 'P', 'W', 'W', 'P', 'P', 'P', 'P', 'W', 'S', 'S', 'P', 'P', 'P', 'P'},
+            {'W', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'W', 'P', 'P', 'P', 'W'}
+        };
+        switchToGame(map); 
     }
 
     @FXML
     private void createMapPD() throws IOException {
-        switchToGame(26, 14); 
+        char[][] map = {
+            {'P', 'P', 'P', 'W', 'W', 'P', 'P', 'P', 'P', 'P', 'P', 'W', 'W', 'W', 'W', 'P', 'P', 'P', 'P', 'P', 'P', 'W', 'W', 'P', 'P', 'P'},
+            {'W', 'P', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'P', 'P'},
+            {'W', 'P', 'T', 'W', 'P', 'P', 'W', 'P', 'P', 'W', 'P', 'W', 'T', 'T', 'W', 'P', 'P', 'W', 'P', 'P', 'W', 'P', 'P', 'T', 'P', 'W'},
+            {'P', 'P', 'T', 'P', 'W', 'P', 'P', 'P', 'W', 'S', 'S', 'W', 'T', 'T', 'W', 'S', 'S', 'W', 'P', 'W', 'P', 'P', 'P', 'T', 'P', 'W'},
+            {'P', 'P', 'T', 'P', 'P', 'P', 'P', 'W', 'S', 'S', 'S', 'W', 'T', 'T', 'W', 'S', 'S', 'S', 'W', 'P', 'P', 'P', 'P', 'T', 'P', 'P'},
+            {'S', 'S', 'T', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'P', 'T', 'T', 'P', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'T', 'S', 'S'},
+            {'S', 'S', 'T', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'P', 'T', 'T', 'P', 'P', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'T', 'S', 'S'},
+            {'S', 'S', 'T', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'W', 'T', 'T', 'P', 'W', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'T', 'S', 'S'},
+            {'S', 'S', 'T', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'P', 'T', 'T', 'P', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'T', 'S', 'S'},
+            {'P', 'P', 'T', 'P', 'W', 'W', 'W', 'P', 'S', 'S', 'S', 'W', 'T', 'T', 'W', 'S', 'S', 'S', 'P', 'W', 'W', 'W', 'P', 'T', 'P', 'P'},
+            {'P', 'P', 'T', 'P', 'P', 'P', 'P', 'P', 'P', 'S', 'S', 'W', 'T', 'T', 'W', 'S', 'S', 'P', 'W', 'P', 'P', 'P', 'P', 'T', 'P', 'P'},
+            {'W', 'P', 'T', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'W', 'T', 'T', 'W', 'P', 'P', 'W', 'P', 'P', 'P', 'P', 'P', 'T', 'P', 'W'},
+            {'W', 'P', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'P', 'W'},
+            {'P', 'P', 'P', 'W', 'W', 'P', 'P', 'P', 'P', 'P', 'P', 'W', 'W', 'W', 'W', 'P', 'P', 'P', 'P', 'P', 'P', 'W', 'W', 'P', 'P', 'P'}
+        };
+        switchToGame(map); 
     }
 
-    private void switchToGame(int rows, int cols) throws IOException {
+    private void switchToGame(char[][] map) throws IOException {
         FXMLLoader loader = Main.getLoader("gameView");
         GameController controller = loader.getController();
-        controller.createGrid(rows, cols);
+        controller.createGrid(map);
 
         Main.setScene(loader);
     }
