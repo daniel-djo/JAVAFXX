749d045c8df85403ce85a0ea052ddce219a416d4
Daniel <s.daniel@djokic.eu>
2024-07-11 16:03:51 +0200

    added fight png, deleted unnecessary addons, changed road png

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,14 +3,6 @@ module de.game {
     requires javafx.fxml;
     requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
-
     opens de.game to javafx.fxml;
     exports de.game;
     exports de.game.controllers;
